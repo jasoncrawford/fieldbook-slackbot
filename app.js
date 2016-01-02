dc1efb81b371c08c6229c4881bb8d47bbb92d116
Jason Crawford <jason@jasoncrawford.org>
2016-01-02 13:28:37 -0800

    Set up stub command handler
    Just logs commands and responds with “OK”.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function (req, res) {
   res.send("Fieldbook Slackbot");
+})
+
+app.post('/commands', function (req, res) {
+  console.log('got command', req.body);
+  res.send("OK");
 })
 
 var port = process.env['PORT'] || 3000;
