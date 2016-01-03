00855afc6c512abb7471dee3eff9ef4b7e3b276a
Jason Crawford <jason@jasoncrawford.org>
2016-01-02 20:34:42 -0800

    Actually look up items from a Fieldbook sheet
    Fieldbook URL is just hardcoded for now

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var requestify = require('requestify');
 
 var app = express();
 
@@ -18,10 +19,21 @@ app.use('/commands', function (req, res, next) {
   }
 })
 
-app.post('/commands', function (req, res) {
+app.post('/commands', function (req, res, next) {
   var body = req.body;
   console.log(`got command from ${body.user_name}: ${body.command} ${body.text}`);
-  res.send("OK");
+
+  requestify.get('https://api.fieldbook.com/v1/568896651bfd500300fa9a5f/items', {
+    headers: {accept: 'application/json'},
+    auth: {username: process.env['FIELDBOOK_KEY'], password: process.env['FIELDBOOK_SECRET']}
+  }).then(function (response) {
+    var items = JSON.parse(response.body);
+    var lines = items.map(row => `${row.id} ${row.name}`);
+    res.send(lines.join('\n'));
+  }).fail(function (error) {
+    console.error('error handling command', error);
+    next(error);
+  })
 })
 
 var port = process.env['PORT'] || 3000;
